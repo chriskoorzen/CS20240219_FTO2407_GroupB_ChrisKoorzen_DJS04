2dae59eabd96df808d9dd6413152099ae30732cc
Chris Koorzen <fckoorzen@gmail.com>
2024-10-23 21:53:43 +0200

    Generate component layout.

--- components.js
+++ components.js
@@ -7,6 +7,46 @@ class BookDisplay extends HTMLDialogElement {
     }
 
     connectedCallback() {
+        const shadow = this.attachShadow({ mode: "closed" })    // Encapsulate this component completely
+
+        this.container = Object.assign(document.createElement("div"), {className: "backdrop"})
+        this.modal = Object.assign(document.createElement("dialog"), {className: "overlay"})
+
+        const imgWrapper = Object.assign(document.createElement("div"), {className: "overlay__preview"})
+        this.background = Object.assign(document.createElement("img"), {className: "overlay__blur"})
+        this.image = Object.assign(document.createElement("img"), {className: "overlay__image"})
+        imgWrapper.append(
+            this.background,
+            this.image
+        )
+
+        const textWrapper = Object.assign(document.createElement("div"), {className: "overlay__content"})
+        this.titleHeading = Object.assign(document.createElement("h3"), {className: "overlay__title"})
+        this.subtitle = Object.assign(document.createElement("div"), {className: "overlay__data"})
+        this.description = Object.assign(document.createElement("p"), {className: "overlay__data overlay__data_secondary"})
+        textWrapper.append(
+            this.titleHeading,
+            this.subtitle,
+            this.description
+        )
+
+        const buttonWrapper = Object.assign(document.createElement("div"), {className: "overlay__row"})
+        const closeButton = Object.assign(document.createElement("button"), {className: "overlay__button overlay__button_primary", innerText: "Close"})
+        closeButton.addEventListener("click", () => {
+            this.modal.close()
+            this.container.style.display = "none"
+        })
+        buttonWrapper.append(
+            closeButton
+        )
+
+        this.modal.append(
+            imgWrapper,
+            textWrapper,
+            buttonWrapper
+        )
+        this.container.append(this.modal)
+        shadow.append(this.container)
 
     }
 
