ab2316ee444228d6be5f751f2a19119a17c9af4e
Chris Koorzen <fckoorzen@gmail.com>
2024-10-23 21:56:47 +0200

    Add encapsulated component styling.

--- components.js
+++ components.js
@@ -1,6 +1,151 @@
 
 class BookDisplay extends HTMLDialogElement {
 
+    static #cssStyles = `
+    * {
+        box-sizing: border-box;
+    }
+
+    @keyframes enter {
+        from {
+            transform: translateY(10rem);
+        }
+        to {
+            transform: translateY(0);
+        }
+    }
+    
+    /* overlay */
+
+    .overlay {
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        border-width: 0;
+        box-shadow: 0px 3px 3px -2px rgba(0,0,0,0.2), 0px 3px 4px 0px rgba(0,0,0,0.14), 0px 1px 8px 0px rgba(0,0,0,0.12);
+        animation-name: enter;
+        animation-duration: 0.6s;
+        z-index: 10;
+        background-color: rgba(var(--color-light), 1);
+    }
+
+    @media (min-width: 30rem) {
+        .overlay {
+            max-width: 30rem;
+            left: 0%;
+            top: 0;
+            border-radius: 8px;;
+        }
+    }
+
+    .overlay__row {
+        display: flex;
+        gap: 0.5rem;
+        margin: 0 auto;
+        justify-content: center;
+    }
+
+    .overlay__button {
+        font-family: Roboto, sans-serif;
+        background-color: rgba(var(--color-blue), 0.1);
+        transition: background-color 0.1s;
+        border-width: 0;
+        border-radius: 6px;
+        height: 2.75rem;
+        cursor: pointer;
+        width: 50%;
+        color: rgba(var(--color-blue), 1);
+        font-size: 1rem;
+        border: 1px solid rgba(var(--color-blue), 1);
+    }
+
+    .overlay__button_primary {
+        background-color: rgba(var(--color-blue), 1);
+        color: rgba(var(--color-force-light), 1);
+    }
+
+    .overlay__button:hover {
+        background-color: rgba(var((var(--color-blue))), 0.2);
+    }
+
+
+    .overlay__button_primary:hover {
+        background-color: rgba(var(--color-blue), 0.8);
+        color: rgba(var(--color-force-light), 1);
+    }
+
+    .overlay__title {
+        padding: 1rem 0 0.25rem;
+        font-size: 1.25rem;
+        font-weight: bold;
+        line-height: 1;
+        letter-spacing: -0.1px;
+        max-width: 25rem;
+        margin: 0 auto;
+        color: rgba(var(--color-dark), 0.8)
+    }
+
+    .overlay__blur {
+        width: 100%;
+        height: 200px;
+        filter: blur(10px);
+        opacity: 0.5;
+        transform: scale(2);
+    }
+
+    .overlay__image {
+        max-width: 10rem;
+        position: absolute;
+        top: 1.5m;
+        left: calc(50% - 5rem);
+        border-radius: 2px;
+        box-shadow: 0px 3px 3px -2px rgba(0,0,0,0.2), 0px 3px 4px 0px rgba(0,0,0,0.14), 0px 1px 8px 0px rgba(0,0,0,0.12);
+    }
+
+    .overlay__data {
+        font-size: 0.9rem;
+        display: -webkit-box;
+        -webkit-line-clamp: 6;
+        -webkit-box-orient: vertical;  
+        overflow: hidden;
+        color: rgba(var(--color-dark), 0.8)
+    }
+
+    .overlay__data_secondary {
+        color: rgba(var(--color-dark), 0.6)
+    }
+
+    .overlay__content {
+        padding: 2rem 1.5rem;
+        text-align: center;
+        padding-top: 3rem;
+    }
+
+    .overlay__preview {
+        overflow: hidden;
+        margin: -1rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+        
+    /* backdrop */
+
+    dialog::backdrop {
+        background-color: rgba(var(--color-dark), 0.3);
+    }
+
+    .backdrop {
+        display: none;
+        background: rgba(var(--color-dark), 0.3);
+        position: fixed;
+        top: 0;
+        left: 0;
+        height: 100vh;
+        width: 100vw;
+    }`
+
     constructor(){
         super()
 
@@ -47,6 +192,12 @@ class BookDisplay extends HTMLDialogElement {
         )
         this.container.append(this.modal)
         shadow.append(this.container)
+
+
+        // Set up Styling
+        const sheet = new CSSStyleSheet();
+        sheet.replaceSync(BookDisplay.#cssStyles)
+        shadow.adoptedStyleSheets = [sheet]
 
     }
 
